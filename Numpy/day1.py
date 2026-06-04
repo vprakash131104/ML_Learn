1dc91162157f4374332cc80c4cfaa1040ee30c08
vprakash131104 <vprakash131104@gmail.com>
2026-06-04 09:47:18 +0530

    Add Numpy Topic

---
+++ Numpy/day1.py
@@ -0,0 +1,15 @@
+
+"""Create a Numpy Array"""
+import numpy as np
+
+arr=np.array([1,2,3])
+print(arr)
+
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr)
+
+arr=np.array((1,3,2))
+print(arr)
+
+"""Accessing the Array index"""
+
---
+++ Numpy/day2.py
@@ -0,0 +1,9 @@
+"""Basic Array Operations"""
+
+import numpy as np
+
+a=np.array([[1,2],[3,4]])
+
+b=np.array([[4,3],[2,1]])
+
+print(" Adding 1 to every element:",a+1)
